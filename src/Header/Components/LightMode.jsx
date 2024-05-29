aff23552f65c7caeade5efa5bacdde02e3e9686e
Nikkhil420 <nikhilbujade419@gmail.com>
2024-05-29 11:13:29 +0530

    First Commit Home Page

---
+++ src/Header/Components/LightMode.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import light from  '../Images/mode.png'
+
+const LightMode = () => {
+  return (
+    <div>
+ <img className='mode' src={light}  />
+
+    </div>
+  )
+}
+
+export default LightMode
